8e76484f08d3236c90238162fd66843fa6e54360
daniel fernandez <daniel.fernandez@itglobers.com>
2023-12-15 16:47:09 -0500

    feat: add socket.emmit

--- react/components/index.jsx
+++ react/components/index.jsx
@@ -21,7 +21,7 @@ import './Chat.css';
 import io from "socket.io-client"
 
 
-const socket = io('http://localhost:4000')
+const socket = io('http://localhost:40001')
 
 const ChatAi = () => {
   const CSS_HANDLES = [
